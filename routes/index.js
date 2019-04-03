24bbbf49f6c0ef081bdd651d56e49d4588454d08
Michael Kohler <me@michaelkohler.info>
2019-04-03 20:59:48 +0200

    Connect GitHub

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,8 @@
 const debug = require('debug')('mozilla-fenix-campaign:routes');
 const express = require('express');
 const formHandling = require('../lib/formHandling');
+const githubBackend = require('../lib/githubBackend');
+
 const router = express.Router();
 
 router.get('/', async (req, res) => {
@@ -10,15 +12,16 @@ router.get('/', async (req, res) => {
   res.render('index', {});
 });
 
-router.post('/create', (req, res) => {
+router.post('/create', async (req, res) => {
   debug('INCOMING_REQUEST_CREATE');
-  formHandling.parseForm(req)
-    .then((fields) => {
-      res.json({ success: true, fields });
-    })
-    .catch((err) => {
-      res.json({ success: false, err });
-    });
+  try {
+    const fields = await formHandling.parseForm(req);
+    const issueInfo = await githubBackend.createIssue(fields);
+    // TODO: redirect to form again with "thanks" and link to issue
+    res.json({ success: true, fields, issueInfo });
+  } catch(error) {
+    res.json({ success: false, error });
+  }
 });
 
 module.exports = router;
