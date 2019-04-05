6c0a224e28d5eaa46be5cc1f01e180d5eb4b8149
Michael Kohler <me@michaelkohler.info>
2019-04-06 01:09:58 +0200

    Automate UA and Android Version

---
+++ public/client.js
@@ -0,0 +1,18 @@
+'use strict';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const fenixVersionInput = document.querySelector('#fenix');
+  fenixVersionInput.value = navigator.userAgent;
+
+  const androidVersionInput = document.querySelector('#android');
+  const androidVersion = getAndroidVersion();
+  if (androidVersion) {
+    androidVersionInput.value = androidVersion;
+  }
+});
+
+function getAndroidVersion(ua = navigator.userAgent) {
+  const userAgent = ua.toLowerCase();
+  const match = userAgent.match(/android\s([0-9]+\.*)*/i);
+  return match;
+};
